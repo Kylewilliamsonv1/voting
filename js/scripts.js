d1d9f93bbb88511bfde9acfd9145bdbf743cbf84
Kylewilliamsonv1 <84644678+Kylewilliamsonv1@users.noreply.github.com>
2021-06-03 11:04:56 -0700

    reconstruct scripts + HTML
    Co-authored-by: Brandon Eads brandon181989@hotmail.com

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,10 @@
-$(document).ready() {
-  const nameInput = $("input#name").val();
-  const ageInput = $("input#age").val();
-
-  $(".name").text(nameInput);
-  $(".age").text(ageInput);
-
-  if (age >= 18) {
-    $('#overage').show();
-  } else {
-    $('#underage').show();
-  }
+$(document).ready(function() {
+    const ageInput = parseInt(prompt("Enter you age"));
+    if (ageInput >= 18) {
+      $('#overage').show();
+    } else {
+      $('#underage').show();
+    }
 });
 
 
